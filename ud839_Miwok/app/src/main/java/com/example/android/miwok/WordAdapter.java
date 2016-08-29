b4e4644f28414fe4232ff7642c7a92b38b33e2ba
Dillon Foley <foleydil@gmail.com>
2016-08-28 20:43:21 -0400

    Add custom adapter WordAdapter for custom Word class. Also add inflators for the NumbersActivity

---
+++ ud839_Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,41 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dillon on 8/28/2016.
+ */
+public class WordAdapter extends ArrayAdapter<Word>{
+
+    private static final String LOG_TAG = WordAdapter.class.getSimpleName();
+
+    public WordAdapter(Activity context, ArrayList<Word> words){
+        super(context,0,words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getmMiwokTranslation());
+        
+        return listItemView;
+    }
+}
