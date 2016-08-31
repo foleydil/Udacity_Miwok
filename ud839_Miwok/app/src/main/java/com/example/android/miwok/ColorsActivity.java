bfea12e6a43762877f3a8534974557df5408eab2
Dillon Foley <foleydil@gmail.com>
2016-08-31 19:17:13 -0400

    Correct spelling on some words

--- ud839_Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ ud839_Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,14 +15,14 @@ public class ColorsActivity extends AppCompatActivity {
 
         //Create ArrayList of words
         ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("red","wetetti"));
+        words.add(new Word("red","weṭeṭṭi"));
         words.add(new Word("green","chokokki"));
-        words.add(new Word("brown","takaakki"));
-        words.add(new Word("gray","topoppi"));
+        words.add(new Word("brown","ṭakaakki"));
+        words.add(new Word("gray","ṭopoppi"));
         words.add(new Word("black","kululli"));
         words.add(new Word("white","kelelli"));
-        words.add(new Word("dusty yellow","topiise"));
-        words.add(new Word("mustard yellow", "chiwiite"));
+        words.add(new Word("dusty yellow","ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
 
         WordAdapter adapter = new WordAdapter(this, words);
 
