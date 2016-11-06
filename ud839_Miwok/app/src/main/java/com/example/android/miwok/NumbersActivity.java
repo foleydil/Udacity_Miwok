ac6aa2ad52f00d8380d37d87ec4445b1bab1bd42
Dillon Foley <foleydil@gmail.com>
2016-11-06 17:31:30 -0500

    Convert ColorsActivity to fragment

--- ud839_Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -9,7 +9,7 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
-       getSupportFragmentManager().beginTransaction()
+        getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, new NumbersFragment())
                 .commit();
     }
