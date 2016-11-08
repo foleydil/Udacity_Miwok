1fdd4aaf97da33e9c6679143df452d6f0b5c0650
Dillon Foley <foleydil@gmail.com>
2016-11-07 21:13:32 -0500

    Implement ViewPager in MainActivity.java

---
+++ ud839_Miwok/app/src/main/java/com/example/android/miwok/CategoryPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Dillon on 11/7/2016.
+ */
+
+public class CategoryPagerAdapter extends FragmentPagerAdapter {
+    final int num_items = 4;
+    Context context;
+
+    public CategoryPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return num_items;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return context.getString(R.string.category_numbers);
+            case 1:
+                return context.getString(R.string.category_family);
+            case 2:
+                return context.getString(R.string.category_colors);
+            case 3:
+                return context.getString(R.string.category_phrases);
+            default:
+                return null;
+        }
+    }
+}
